c71114966e0930a075565c9160a8c27e21f93f4f
Edward Amsden <edwardamsden@gmail.com>
2013-03-28 23:34:40 -0400

    Fix parsing when extra characters are on the end of a time, like 's' for seconds)

--- analyze/DatfileHelpers.hs
+++ analyze/DatfileHelpers.hs
@@ -11,7 +11,7 @@ import Text.PrettyPrint.HughesPJClass
 import Debug.Trace (trace)
 import Data.Maybe (mapMaybe)
 import Text.Regex
-import Data.Char (isSpace)
+import Data.Char (isSpace, isDigit)
 
 --------------------------------------------------------------------------------
 -- Data Types:
@@ -44,26 +44,28 @@ parse :: [String] -> Maybe Entry
 -- Example data line:
 -- sorting/mergesort                   cpu_24_8192          SMP     28   4.459116 4.459116 4.459116   68.062 68.062 68.062
 parse orig@(name:args:sched:thrds:t1:t2:t3:prods) = 
-   case prods of
+  case prods of
     []         -> Just$ defaultRec 
     [p1,p2,p3] -> Just$ defaultRec { productivities = Just (read p1, read p2, read p3) }
     ls         -> 
                   -- For now it's a warning rather an error if one data-point doesn't parse:
                   trace ("WARNING: Cannot parse, wrong number of fields, "++ 
-			 show (length orig) ++" expected 7 or 10:\n  "++ show (unwords orig)) $ 
-		  Nothing
+                  show (length orig) ++" expected 7 or 10:\n  "++ show (unwords orig))
+                  Nothing
   where 
    defaultRec =
     Entry { name     = name, 
 	    variant  = args,
 	    sched    = sched,
 	    threads  = read thrds,
-	    tmin     = read t1,
-	    tmed     = read t2,
-	    tmax     = read t3,
+	    tmin     = read $ filterForDouble t1,
+	    tmed     = read $ filterForDouble t2,
+	    tmax     = read $ filterForDouble t3,
 	    productivities = Nothing,
 	    normfactor = 1.0
 	  }
+   filterForDouble = filter (\c -> isDigit c || c == '.')
+
 -- Some older data entries look like this:
 -- "queens Trace 4 3.71 3.76 3.78"
 parse [name, sched, thrds, t1, t2, t3] = Just$ 
