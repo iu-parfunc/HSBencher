7f183d9006e5f9ed2ff746022b2f76e021df414a
Ryan Newton <rrnewton@gmail.com>
2013-05-28 11:30:12 -0400

    Add convenience module, HSBencher

---
+++ HSBencher.hs
@@ -0,0 +1,13 @@
+
+
+-- | Convenience module that reexports the necessary bits.
+
+module HSBencher
+       (
+         module HSBencher.App,
+         module HSBencher.Types
+       )
+       where
+
+import HSBencher.App
+import HSBencher.Types
