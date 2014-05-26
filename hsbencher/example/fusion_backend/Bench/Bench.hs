74a802d9ccd7c9090f5ca5004be59ca082e1a71c
Joel Svensson <svenssonjoel@yahoo.se>
2014-05-26 12:22:35 -0400

    added fusion backend example

---
+++ hsbencher/example/fusion_backend/Bench/Bench.hs
@@ -0,0 +1,8 @@
+
+
+main = do
+  putStrLn "Hello World!"
+  putStrLn "SELFTIMED: 3.14"
+  putStrLn "MYTAG: 1000"
+  putStrLn "ANOTHERTAG: 1.2"
+  
---
+++ hsbencher/example/fusion_backend/run_benchmark.hs
@@ -0,0 +1,51 @@
+#!/usr/bin/env runghc
+{-# LANGUAGE NamedFieldPuns #-}
+
+import System.Directory
+import System.FilePath
+import System.Exit
+import System.Environment (getArgs)
+
+import GHC.Conc (getNumProcessors)
+import System.IO.Unsafe (unsafePerformIO)
+
+import qualified Data.ByteString.Char8 as B
+import Data.Monoid 
+
+import HSBencher
+import HSBencher.Harvesters
+import HSBencher.Internal.MeasureProcess
+import HSBencher.Backend.Fusion 
+
+import Prelude hiding (log)
+--------------------------------------------------------------------------------
+
+main :: IO ()
+main = defaultMainModifyConfig myconf
+
+all_benchmarks :: [Benchmark DefaultParamMeaning]
+all_benchmarks =
+  [  mkBenchmark "Bench/Bench.cabal" [] defaultCfgSpc ]
+  
+-- | Default configuration space over which to vary settings:
+--   This is a combination of And/Or boolean operations, with the ability
+--   to set various environment and compile options.
+defaultCfgSpc = And []
+
+-- Interface needs some work
+--customHarvester =staggedLineHarvester (B.pack "MYTAG") (\d r -> r {custom = ("MYTAG",IntResult d) : custom r})
+--customHarvester1 = taggedLineHarvester (B.pack "ANOTHERTAG") (\d r -> r {custom= ("ANOTHERTAG",DoubleResult d) : custom r})
+
+-- | Here we have the option of changing the HSBencher config
+myconf :: Config -> Config
+myconf conf =
+  conf
+   { benchlist = all_benchmarks,
+     plugIns = [SomePlugin defaultFusionPlugin],
+     -- harvesters = customHarvester1 `mappend` customHarvester `mappend` (harvesters conf)
+     harvesters = customTagHarvesterInt "MYTAG" `mappend`
+                  customTagHarvesterDouble "ANOTHERTAG" `mappend`
+                  harvesters conf
+   }
+
+
