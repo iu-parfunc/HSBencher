e92cba9085e534dda7c76019fa035fb90a3255bb
Ryan Newton <rrnewton@gmail.com>
2014-07-29 10:50:50 -0400

    Fix jenkins script

--- .jenkins_script.sh
+++ .jenkins_script.sh
@@ -4,13 +4,11 @@ if [ "$CABAL" == "" ]; then
  CABAL=cabal
 fi
 
-PKGS=" ./hsbencher/ ./hsbencher-fusion/ ./hgdata "
-
 $CABAL --version
 git submodule update --init
 $CABAL sandbox init
-$CABAL install --only-dependencies $PKGS -j
-$CABAL install $PKGS --enable-tests 
+$CABAL install --only-dependencies ./hsbencher/ ./hsbencher-fusion/ -j
+$CABAL install ./hsbencher/ ./hsbencher-fusion/ --enable-tests 
 # --show-details=streaming
 
 # cabal install -ffusion --enable-tests ./ http-conduit-1.9.6 handa-gdata-0.6.9.1 --force-reinstalls 
