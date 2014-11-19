09bf1d8c96a80ac75b20687c608a3d3168d1bfb9
Ryan Newton <rrnewton@gmail.com>
2014-11-19 16:53:18 -0500

    Add filtration functionality based on BenchSpace.  Partially addresses issue #50

--- hsbencher/HSBencher/Internal/App.hs
+++ hsbencher/HSBencher/Internal/App.hs
@@ -13,7 +13,8 @@
 
 module HSBencher.Internal.App
        (defaultMainModifyConfig,
-        Flag(..), all_cli_options, fullUsageInfo)
+        Flag(..), all_cli_options, fullUsageInfo
+       )
        where 
 
 ----------------------------
@@ -27,7 +28,8 @@ import Data.IORef
 import Data.List (intercalate, sortBy, intersperse, isInfixOf)
 import qualified Data.List as L
 import qualified Data.Map as M
-import Data.Maybe (isJust, fromJust, fromMaybe)
+import qualified Data.Set as S
+import Data.Maybe (isJust, fromJust, fromMaybe, mapMaybe)
 import Data.Version (versionBranch)
 import Data.Word (Word64)
 import Numeric (showFFloat)
@@ -482,7 +484,6 @@ defaultMain = do
   --      benchF = get "BENCHLIST" "benchlist.txt"
 --  putStrLn$ hsbencher_tag ++ " Reading benchmark list from file: "
   error "FINISHME: defaultMain requires reading benchmark list from a file.  Implement it!"
---  defaultMainWithBenchmarks undefined
 
 -- | In this version, user provides a list of benchmarks to run, explicitly.
 defaultMainWithBenchmarks :: [Benchmark DefaultParamMeaning] -> IO ()
@@ -625,18 +626,7 @@ defaultMainModifyConfig modConfig = do
 
   -------------------------------------------------------------------
   -- Next prune the list of benchmarks to those selected by the user:
-  let cutlist = case plainargs of
-                 [] -> benchlist conf3
-                 patterns -> filter (\ Benchmark{target,cmdargs,progname} ->
-                                      -- [2014.11.19] Changed 'any' to all here as part of fixing issue #50:
-                                      all (\pat ->
-                                            isInfixOf pat target ||
-                                            isInfixOf pat (fromMaybe "" progname) ||
-                                            -- FIXME: this isn't all of the args, could be RuntimeArg:
-                                            any (isInfixOf pat) cmdargs
-                                          )
-                                          patterns)
-                                    (benchlist conf3)
+  let cutlist = filterBenchmarks plainargs (benchlist conf3)
   let conf4@Config{extraParams} = conf3{benchlist=cutlist}
 
   -- Finally, put the extra Params right into the benchmark config
@@ -682,7 +672,7 @@ defaultMainModifyConfig modConfig = do
             
         log$ "\n--------------------------------------------------------------------------------"
         logT$ "Running all benchmarks for all settings ..."
-        logT$ "Compiling: "++show totalcomps++" total configurations of "++ show (length benchlist)++" benchmarks"
+        logT$ "Compiling: "++show totalcomps++" total configurations of "++ show (length cutlist) ++" benchmarks"
         let indent n str = unlines $ map (replicate n ' ' ++) $ lines str
             printloop _ [] = return ()
             printloop mp (Benchmark{target,cmdargs,configs} :tl) = do
@@ -837,6 +827,87 @@ defaultMainModifyConfig modConfig = do
 	liftIO$ exitSuccess
     )
     conf5
+
+-- Semantics:
+--   A benchmark (+configs) is treated really a STREAM of concrete benchmark instances.
+-- 
+--   Each pattern filters out a subset of these instances (either by
+--   matching a varying or a static field).  This function returns a
+--   benchmark that iterates over the INTERSECTION of those subsets.
+-- 
+filterBenchmarks :: [String]  -- ^ Patterns which must all be matched.
+                 -> [Benchmark DefaultParamMeaning] -> [Benchmark DefaultParamMeaning]
+filterBenchmarks [] = id -- Don't convert to disjunctive normal form!
+filterBenchmarks patterns = mapMaybe fn
+  where
+   fn orig@Benchmark{target,cmdargs,progname,configs} = 
+     let unmet = [ pat | pat <- patterns
+                       , not (isInfixOf pat target ||
+                              isInfixOf pat (fromMaybe "" progname) ||
+                              any (isInfixOf pat) cmdargs) ]
+     in case filtConfigs unmet configs of
+          Or [] -> Nothing
+          newcfgs -> Just (orig { configs = newcfgs } )
+               
+-- Returns Or [] for an empty config space.
+filtConfigs :: Show a => [String] -> BenchSpace a -> BenchSpace a
+filtConfigs pats bs =
+  let Or ls = dnf bs
+  in Or [ And as | And as <- ls, andMatch pats as ]
+
+-- Does a conjoined sequence of Set clauses match ALL patterns?
+andMatch :: Show a => [String] -> [BenchSpace a] -> Bool
+andMatch pats0 ls = null (f pats0 ls)
+ where 
+  f []   _  = []
+  f pats [] = pats
+  f pats (x@Set{} : rst) = let pats' = g pats x
+                           in f pats' rst
+  g [] Set{} = []
+  g (hd:pats) (Set ls1 ls2) =
+    if isInfixOf hd (show ls1) || isInfixOf hd (show ls2)
+    then      g pats (Set ls1 ls2)
+    else hd : g pats (Set ls1 ls2)
+
+
+-- | Convert to disjunctive normal form:
+dnf :: BenchSpace a -> BenchSpace a
+dnf = Or . map And . loop 
+ where
+ loop bs =
+  case bs of
+    Set _ _ -> [[bs]]
+    And []  -> [[]]
+    And (h:t) -> [ x++y | x <- loop h
+                        , y <- loop (And t) ]
+    Or ls -> concatMap loop ls
+
+addAnd :: BenchSpace meaning -> BenchSpace meaning -> BenchSpace meaning
+addAnd (Or []) _  = Or []
+addAnd x (And ls) = And (x:ls)
+addAnd x y        = And [x,y]
+
+addOr :: BenchSpace t -> BenchSpace t -> BenchSpace t
+addOr (Or []) rst = rst
+addOr x (Or ls)   = Or (x:ls)
+addOr x rst       = Or [x,rst]
+
+mkOr :: [BenchSpace meaning] -> BenchSpace meaning
+mkOr [] = Or []
+mkOr (x : tl) = addOr x (mkOr tl)
+
+intersections :: Ord a => [S.Set a] -> S.Set a
+intersections [] = error "No set intersection of the empty list"
+intersections [s] = s
+intersections (s1:sets) = S.intersection s1 (intersections sets)
+
+bp1 :: BenchSpace DefaultParamMeaning
+bp1 = (And [Set (Variant "Reduce") (RuntimeArg "Reduce"),
+            Set NoMeaning (RuntimeArg "r6") ])
+
+t1 = filtConfigs ["Reduce", "r6"] bp1
+
+t2 = filtConfigs ["Reduce", "r6"] (Or [ bp1, Set NoMeaning (RuntimeEnv "FOO" "r6") ])
 
 
 -- Several different options for how to display output in parallel:
